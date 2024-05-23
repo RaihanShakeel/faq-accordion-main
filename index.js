313c4029b8e5402511b2133972489f4aa0179303
RaihanShakeel <raihanshakeel58@gmail.com>
2024-05-23 16:47:44 +0500

    updated index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ let toggleIcons = document.getElementsByClassName('toggle-icon');
 let questions = document.getElementsByClassName('question');
 let lastOpened = null; // Keep track of the last opened answer
 
-function toggleFAQ(icon, answer) {
+function toggleFAQ(icon, answer, question) {
   if (lastOpened && lastOpened !== answer) {
     let lastIcon = lastOpened.closest('.FAQ').querySelector('.toggle-icon');
     lastIcon.src = 'assets/images/icon-plus.svg';
     lastIcon.alt = 'Plus Icon';
     lastOpened.style.display = 'none';
+    question.style.color = 'hsl(292, 42%, 14%)';
   }
 
   if (icon.src.includes('icon-plus.svg')) {
@@ -15,11 +16,14 @@ function toggleFAQ(icon, answer) {
     icon.alt = 'Minus Icon';
     answer.style.display = 'block';
     lastOpened = answer; // Update lastOpened to the current answer
+    question.style.color = 'hsl(223, 69%, 50%)'
+    
   } else {
     icon.src = 'assets/images/icon-plus.svg';
     icon.alt = 'Plus Icon';
     answer.style.display = 'none';
     lastOpened = null; // Reset lastOpened as no answer is open now
+    question.style.color = 'hsl(292, 42%, 14%)';
   }
 }
 
@@ -37,6 +41,6 @@ Array.from(questions).forEach(question => {
     let parent = event.target.closest('.FAQ');
     let icon = parent.querySelector('.toggle-icon');
     let answer = parent.querySelector('.answer');
-    toggleFAQ(icon, answer);
+    toggleFAQ(icon, answer, question);
   });
 });
